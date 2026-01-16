44356c15e785c40f7fbd4784bb3dd843c8469b6a
DenisSolicen <DenisSolicen@yandex.ru>
2026-01-16 03:55:30 +0300

    Миграция на .NET 8 и обновление UAssetAPI до 1.0.2.0

--- KismetEditor/Properties/AssemblyInfo.cs
+++ KismetEditor/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанные с этой сборкой.
-[assembly: AssemblyTitle("KissE")]
-[assembly: AssemblyDescription("Kismet extractor and string replacer.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Solicen")]
-[assembly: AssemblyProduct("KissE")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("Solicen")]
 [assembly: AssemblyCulture("")]
 
@@ -21,13 +11,3 @@ using System.Runtime.InteropServices;
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
 [assembly: Guid("df18a493-2999-4179-9f78-120b3d06f7d7")]
-
-// Сведения о версии сборки состоят из указанных ниже четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
-//      Номер редакции
-//
-[assembly: AssemblyVersion("1.0.4.6")]
-[assembly: AssemblyFileVersion("1.0.4.6")]
